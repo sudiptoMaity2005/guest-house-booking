d7a80da3a4da698e2207157dc6c287ed2c610738
sudipto8143 <maitysudipto7@gmail.com>
2026-03-14 20:31:52 +0530

    Updated Axios baseURL to Render live API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5001/api', 
+    baseURL: 'https://guest-house-booking-v0j1.onrender.com', 
 });
 
 API.interceptors.request.use((req) => {
